3d7d83ce37f4ea3164333f1f491e644de05c702a
arulagrawal <me@arul.io>
2021-11-22 18:24:34 +0400

    fix small mistakes

--- code/DragonCrew.cpp
+++ code/DragonCrew.cpp
@@ -1,70 +1,55 @@
 #include "DragonCrew.h"
 
-DragonCrew::DragonCrew(string cargo[], int cargoSize, string crew[], int crewSize) : Spacecraft(cargo, cargoSize)
-{
+DragonCrew::DragonCrew(string cargo[], int cargoSize, string crew[], int crewSize) : Spacecraft(cargo, cargoSize) {
     this->crewSize = crewSize;
-    for (int i = 0; i < crewSize; i++)
-    {
+    for (int i = 0; i < crewSize; i++) {
         this->crew.push_back(crew[i]);
         this->embark(crew[i]);
     }
 }
 
-DragonCrew::DragonCrew(string cargo[], int cargoSize, vector<string> crew, int crewSize) : Spacecraft(cargo, cargoSize)
-{
+DragonCrew::DragonCrew(string cargo[], int cargoSize, vector<string> crew, int crewSize) : Spacecraft(cargo, cargoSize) {
     this->crew = crew;
     this->crewSize = crewSize;
-    for (int i = 0; i < crewSize; i++)
-    {
+    for (int i = 0; i < crewSize; i++) {
         this->crew.push_back(crew[i]);
         this->embark(crew[i]);
     }
 }
 
-DragonCrew::~DragonCrew()
-{
+DragonCrew::~DragonCrew() {
     //delete crew;
 }
 
-void DragonCrew::boost()
-{
+void DragonCrew::boost() {
     stage->boost();
 }
 
-void DragonCrew::slow()
-{
+void DragonCrew::slow() {
     stage->slow();
 }
 
-void DragonCrew::left()
-{
+void DragonCrew::left() {
     stage->left();
 }
 
-void DragonCrew::right()
-{
+void DragonCrew::right() {
     stage->right();
 }
 
-void DragonCrew::embark(string member)
-{
+void DragonCrew::embark(string member) {
     cout << "Crew member " << member << " entering Spacecraft" << endl;
 }
 
-void DragonCrew::disembark(string member)
-{
+void DragonCrew::disembark(string member) {
     cout << "Crew member " << member << " exciting Spacecraft" << endl;
 }
 
 vector<string> DragonCrew::getPeople() {
-    vector<string> empty;
-    return empty;
+    return this->crew;
 }
-
 
 Spacecraft* DragonCrew::clone() {
-    int arrCargoSize = sizeof(cargo)/sizeof(cargo[0]);
-    int arrCrewSize = sizeof(crew)/sizeof(crew[0]);
-    return new DragonCrew(this->cargo, arrCargoSize, this->crew, arrCrewSize);
+    int arrCargoSize = sizeof(cargo) / sizeof(cargo[0]);
+    return new DragonCrew(this->cargo, arrCargoSize, this->crew, this->crew.size());
 }
-
